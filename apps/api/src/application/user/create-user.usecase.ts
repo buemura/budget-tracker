21546a4220170181706c13c132f5b90f903dadc3
buemura <bruno.uemura1@gmail.com>
2023-08-12 23:33:12 -0300

    fix(api): fixes password hash

--- apps/api/src/application/user/create-user.usecase.ts
+++ apps/api/src/application/user/create-user.usecase.ts
@@ -1,13 +1,18 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 
+import { PasswordHashService } from '@domain/cryptography/contracts';
 import { User } from '@domain/user/entities/user';
 import { ERROR_MESSAGE } from '@domain/user/errors/messages';
 import { ICreateUser } from '@domain/user/interfaces/user.interface';
 import { UserRepository } from '@domain/user/repositories/user.repository';
+import { PASSWORD_HASH_SALT } from '@helpers/constants';
 
 @Injectable()
 export class CreateUserUsecase {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly passwordHashService: PasswordHashService,
+  ) {}
 
   async execute(data: ICreateUser): Promise<User> {
     const user = await this.userRepository.findByEmail(data.email);
@@ -15,6 +20,13 @@ export class CreateUserUsecase {
       throw new ConflictException(ERROR_MESSAGE.USER_ALREADY_EXIST);
     }
 
-    return this.userRepository.create(data);
+    const hashedPassword = this.passwordHashService.hash(
+      data.password,
+      PASSWORD_HASH_SALT,
+    );
+    return this.userRepository.create({
+      ...data,
+      password: hashedPassword,
+    });
   }
 }
